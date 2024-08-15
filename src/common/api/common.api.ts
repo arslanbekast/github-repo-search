1d20f604f9ffd702bfa2eb4d7659ef491715ec11
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-15 18:01:29 +0300

    refactor: change common.api

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+// Чтобы запросы к api работали, нужно создать в корне проекта .env файл,
+// и создать переменную VITE_GITHUB_TOKEN = 'github токен'
+
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN
 
 export const instance = axios.create({
