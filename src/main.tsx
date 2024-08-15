e5bddceebedadfc667bf4a3c3c7c56d12e9a66d7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-15 14:49:33 +0300

    feat: add fonts import and redux provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-
 import { createRoot } from 'react-dom/client'
+import './index.scss'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
-import { App } from '@/App'
+import { App } from '@/app/App'
+import { store } from '@/app/store'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 )
