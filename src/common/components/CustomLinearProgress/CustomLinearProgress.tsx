d56d89888574c872d6abb04ba2ac30e3deb3341c
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-15 14:55:45 +0300

    feat: create CustomLinearProgress component

---
+++ src/common/components/CustomLinearProgress/CustomLinearProgress.tsx
@@ -0,0 +1,9 @@
+import { Box, LinearProgress } from '@mui/material'
+
+export const CustomLinearProgress = () => {
+  return (
+    <Box position={'fixed'} top={0} width={'100%'}>
+      <LinearProgress />
+    </Box>
+  )
+}
