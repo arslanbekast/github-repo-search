9d07e5bd2e2fec110899fd22c6d0c80c1d257de0
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-08-15 15:20:25 +0300

    chore: move index.scss to app/styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+import './app/styles/index.scss'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
